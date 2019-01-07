de5211b36770c560ba386c588b2540940a594ad0
kkacan <33515415+kkacan@users.noreply.github.com>
2019-01-07 15:21:00 +0100

    Add files via upload

---
+++ openwhyd/config.py
@@ -0,0 +1,9 @@
+
+
+class Config:
+	allURL = "https://openwhyd.org/hot/"
+	usersURL = "https://openwhyd.org/api/user"
+	userURL = "https://openwhyd.org/u/"
+	searchURL = "https://openwhyd.org/search"
+	timeout = 10
+	limit = 20
---
+++ openwhyd/main.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_bootstrap import Bootstrap
+import requests
+from datetime import datetime
+from flask_caching import Cache
+from config import Config
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.secret_key = 'blabla'
+bootstrap = Bootstrap(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+# Prijevod
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+babel = Babel(app)
+
+@app.route('/')
+@cache.cached(timeout=Config.timeout)
+def index():
+
+    return genre('')
+
+@app.route('/<id>')
+@cache.cached(timeout=Config.timeout)
+def genre(id):
+    
+    # Priprema parametara
+    parameters = { 'format': 'json', 'limit': Config.limit}
+    
+    # Poziv REST servisa, HTTP GET
+    response = requests.get(Config.allURL + id, params=parameters)
+   
+    # JSON decoder (http://docs.python-requests.org/en/latest/user/quickstart/#json-response-content)
+    jsonMusic = response.json()
+  
+     # Iteracija kroz JSON objekt i formatiranje podataka za prikaz
+    for music in jsonMusic["tracks"]:
+
+        if (music["eId"].startswith('/sc/')) or (music["eId"].startswith('/vi/')):
+               jsonMusic["tracks"].remove(music)
+        
+        music["eId"] = music["eId"].replace('/yt/','')
+      
+    return render_template('index.html', jsonMusic=jsonMusic)
+
+@app.route("/user/<userId>")
+@cache.cached(timeout=Config.timeout)
+def user(userId):
+
+	# Priprema parametara
+    parameters = { 'format': 'json', 'limit': Config.limit}
+	
+	# Poziv REST servisa, HTTP GET
+    response = requests.get(Config.userURL + userId, params=parameters)
+	
+	# JSON decoder (http://docs.python-requests.org/en/latest/user/quickstart/#json-response-content)
+    user = response.json()
+
+    # Iteracija kroz JSON objekt i formatiranje podataka za prikaz
+    for music in user:
+        if (music["eId"].startswith('/sc/')) or (music["eId"].startswith('/vi/')):
+                user.remove(music)
+        
+        music["eId"] = music["eId"].replace('/yt/','')
+ 
+    return render_template("user.html", user=user)
+
+@app.route("/search", methods=['GET', 'POST'])
+@cache.cached(timeout=Config.timeout)
+def search():
+    
+    # Dohvaćanje query argumenta
+    query = request.args.get('query')
+
+    # Provjera da li je prazan query
+    if (query!=""):
+    
+	    # Priprema parametara
+        parameters = { 'context': 'addTrack', 'q': query, 'limit': Config.limit, 'format': 'json' }
+	
+	    # Poziv REST servisa, HTTP GET
+        response = requests.get(Config.searchURL, params=parameters)
+	
+	    # JSON decoder (http://docs.python-requests.org/en/latest/user/quickstart/#json-response-content)
+        search = response.json()
+        
+        # Iteracija kroz JSON objekt i formatiranje podataka za prikaz
+        for music in search:
+            if (music["eId"].startswith('/sc/')) or (music["eId"].startswith('/vi/')):
+                    search.remove(music)
+        
+            music["eId"] = music["eId"].replace('/yt/','')
+  
+    
+        return render_template("search.html", search=search)    
+
+    # Ako je query prazan preusmjeri na index
+    else:
+
+        return redirect(url_for('index'))
+
+@babel.localeselector
+def get_locale():
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+    return session.get('lang', 'hr')
+
+if __name__ == "__main__":
+	app.run()
+
